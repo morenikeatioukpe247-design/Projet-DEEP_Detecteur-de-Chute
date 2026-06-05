d57c7b93e739780f52e5838098990f3cca7b18c7
Raafath Agbékè Omotayo Iyabo Amoussa <raafath.amoussa@reseau.eseo.fr>
2026-06-05 10:56:09 +0200

    Create capteurPouls.h

---
+++ app/capteurPouls.h
@@ -0,0 +1,24 @@
+/*
+ * capteurPouls.h
+ *
+ *  Created on: Mar 25, 2026
+ *      Author: raafa
+ */
+
+
+#ifndef CAPTEURPOULS_H
+#define CAPTEURPOULS_H
+
+#include "stm32g4xx_hal.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+void     Pouls_Init(void);
+uint16_t Pouls_GetRaw(void);
+bool     Pouls_ElectrodesConnectees(void);
+uint8_t  Pouls_GetBPM(void);
+bool Pouls_IsValid(void);
+
+
+#endif /*CAPTEURPOULS_H*/
+
