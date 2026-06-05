7f030949bbe6927feb473d968f206e01ed4f35ec
Raafath Agbékè Omotayo Iyabo Amoussa <raafath.amoussa@reseau.eseo.fr>
2026-06-05 10:25:20 +0200

    Update buzzer.h

--- buzzer.h
+++ buzzer.h
@@ -9,9 +9,12 @@
 #define BUZZER_H_
 #include "stm32g4xx_hal.h"
 #include "stm32g4_gpio.h"
+
+
 void BUZZER_init(void);
 void BUZZER_On(void);
 void BUZZER_Off(void);
+
 #define BUZZER_PIN  GPIO_PIN_4
 #define BUZZER_GPIO GPIOB
 
