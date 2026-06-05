34325350e2a394391a2fcb6f93646efc0397571f
Raafath Agbékè Omotayo Iyabo Amoussa <raafath.amoussa@reseau.eseo.fr>
2026-06-05 10:30:18 +0200

    Update screen.h

--- screen.h
+++ screen.h
@@ -8,6 +8,14 @@
 #ifndef SCREEN_H_
 #define SCREEN_H_
 
+#include "stm32g4xx_hal.h"
+#include <stdint.h>
+#include <stdbool.h>
 
+void Screen_Init(void);
+void Screen_Update(void);
+void Screen_SetState(const char* state);
+void Screen_SetBPM(uint8_t bpm);
+void Screen_ShowElectrodeStatus(bool connected);
 
 #endif /* SCREEN_H_ */
