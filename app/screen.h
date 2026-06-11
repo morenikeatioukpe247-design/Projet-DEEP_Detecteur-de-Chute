32b998e81c5473067a84fb2eeff44e466f8a02bb
Raafath Agbékè Omotayo Iyabo Amoussa <raafath.amoussa@reseau.eseo.fr>
2026-06-11 16:09:14 +0200

    Update screen.h

--- app/screen.h
+++ app/screen.h
@@ -1,8 +1,11 @@
-/*
- * screen.h
+/**
+ * @file    screen.h
+ * @author  raafa
+ * @date    Apr 29, 2026
+ * @brief   Module de gestion de l'affichage sur ecran TFT ILI9341.
  *
- *  Created on: Apr 29, 2026
- *      Author: raafa
+ * Ce module gere l'affichage de l'etat du systeme, du BPM
+ * et du statut des electrodes sur l'ecran TFT ILI9341.
  */
 
 #ifndef SCREEN_H_
@@ -12,10 +15,35 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/**
+ * @brief Initialise l'ecran TFT ILI9341 et efface le fond en noir.
+ */
 void Screen_Init(void);
+
+/**
+ * @brief  Met a jour l'affichage si necessaire.
+ * @note   N'effectue le redessin que si le flag needs_update est leve.
+ */
 void Screen_Update(void);
+
+/**
+ * @brief  Definit l'etat du systeme a afficher.
+ * @param  state : chaine de caracteres decrivant l'etat
+ * @note   Leve le flag needs_update automatiquement.
+ */
 void Screen_SetState(const char* state);
+
+/**
+ * @brief  Definit la valeur BPM a afficher.
+ * @param  bpm : frequence cardiaque en battements par minute
+ * @note   Leve le flag needs_update automatiquement.
+ */
 void Screen_SetBPM(uint8_t bpm);
+
+/**
+ * @brief  Definit le statut de connexion des electrodes.
+ * @param  connected : true si electrodes connectees, false sinon
+ */
 void Screen_ShowElectrodeStatus(bool connected);
 
 #endif /* SCREEN_H_ */
