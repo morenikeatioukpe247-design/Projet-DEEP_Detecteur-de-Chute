c299480381c129a7f04b968b71d872f0734eabe9
Raafath Agbékè Omotayo Iyabo Amoussa <raafath.amoussa@reseau.eseo.fr>
2026-06-11 16:11:24 +0200

    Update led.h

--- app/led.h
+++ app/led.h
@@ -1,23 +1,29 @@
-/*
- * led.h
+/**
+ * @file    led.h
+ * @author  raafa
+ * @date    Mar 25, 2026
+ * @brief   Module de gestion de la LED indicatrice de mode jour/nuit.
  *
- *  Created on: Mar 25, 2026
- *      Author: raafa
+ * Ce module gere la LED bleue connectee sur PA11 qui indique
+ * le mode jour (eteinte) ou nuit (allumee).
  */
 
 #ifndef LED_H_
 #define LED_H_
+
 #include "stm32g4xx_hal.h"
 #include "stm32g4_gpio.h"
 
+#define LED_BLUE_PIN    GPIO_PIN_11
 
-#define LED_BLUE_PIN		GPIO_PIN_11
-#define LED_BLUE_GPIO       GPIOA
+#define LED_BLUE_GPIO   GPIOA
 
 void LED_init(void);
+
 void LED_setJour(void);
+
 void LED_setNuit(void);
-void BSP_GPIO_pin_config(GPIO_TypeDef *GPIOx, uint32_t GPIO_Pin, uint32_t GPIO_Mode, uint32_t GPIO_Pull, uint32_t GPIO_Speed, uint32_t GPIO_Alternate);
 
 void LED_BLINKY(uint16_t ms);
+
 #endif /* LED_H_ */
