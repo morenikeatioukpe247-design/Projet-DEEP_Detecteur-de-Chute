f8be410af303878536534a129fc4d9f75ca20a85
Raafath Agbékè Omotayo Iyabo Amoussa <raafath.amoussa@reseau.eseo.fr>
2026-06-11 16:14:41 +0200

    Update stm32g4_ad8232.c

--- app/drivers/bsp/AD8232/stm32g4_ad8232.c
+++ app/drivers/bsp/AD8232/stm32g4_ad8232.c
@@ -11,8 +11,8 @@
 
 void AD8232_init(void)
 {
-    // LO+ sur PA3, LO- sur PA12
-    BSP_GPIO_pin_config(GPIOA, GPIO_PIN_3,  GPIO_MODE_INPUT, GPIO_NOPULL, GPIO_SPEED_FREQ_LOW, GPIO_NO_AF);
+    // LO+ sur PB5, LO- sur PA12
+    BSP_GPIO_pin_config(GPIOB, GPIO_PIN_5,  GPIO_MODE_INPUT, GPIO_NOPULL, GPIO_SPEED_FREQ_LOW, GPIO_NO_AF);
     BSP_GPIO_pin_config(GPIOA, GPIO_PIN_12, GPIO_MODE_INPUT, GPIO_NOPULL, GPIO_SPEED_FREQ_LOW, GPIO_NO_AF);
 }
 
@@ -24,7 +24,7 @@ uint16_t AD8232_getRaw(void)
 bool AD8232_electrodesConnectees(void)
 {
     // Affiche l'état des broches pour déboguer
-    uint8_t lo_plus  = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3);
+    uint8_t lo_plus  = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5);
     uint8_t lo_minus = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_12);
     printf("LO+ = %d | LO- = %d\n", lo_plus, lo_minus);
 
